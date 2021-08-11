89cf68b88e957646dc88f4f31ab7e3c30c43a56e
Dominick Caponi <d.caponi1@gmail.com>
2021-08-11 07:55:50 -0700

    makes NewRedisCache follow go idiom of returning structs not interfaces

--- cache/cache.go
+++ cache/cache.go
@@ -20,35 +20,40 @@ type Cache interface {
 type RedisCacheConfig struct {
 	Addr, Password string
 	DB             int
-	Client         *redis.Client
 }
 
-func NewRedisCache(c RedisCacheConfig) (Cache, error) {
-	c.Client = redis.NewClient(&redis.Options{
-		Addr:     c.Addr,
-		Password: c.Password,
-		DB:       c.DB,
-	})
-	_, err := c.Client.Ping(ctx).Result()
+type RedisCache struct {
+	Client *redis.Client
+}
+
+func NewRedisCache(c RedisCacheConfig) (RedisCache, error) {
+	cache := RedisCache{
+		Client: redis.NewClient(&redis.Options{
+			Addr:     c.Addr,
+			Password: c.Password,
+			DB:       c.DB,
+		}),
+	}
+	_, err := cache.Client.Ping(ctx).Result()
 	if err != nil {
-		return nil, err
+		return cache, err
 	}
 	log.Println("established connection to redis!")
-	return c, nil
+	return cache, nil
 }
 
-func (c RedisCacheConfig) Get(key string) (string, error) {
+func (c RedisCache) Get(key string) (string, error) {
 	return c.Client.Get(ctx, key).Result()
 }
 
-func (c RedisCacheConfig) Set(key string, val interface{}) error {
+func (c RedisCache) Set(key string, val interface{}) error {
 	return c.Client.Set(ctx, key, val, 0).Err()
 }
 
-func (c RedisCacheConfig) Setex(key string, val interface{}, ttl time.Duration) error {
+func (c RedisCache) Setex(key string, val interface{}, ttl time.Duration) error {
 	return c.Client.Set(ctx, key, val, ttl).Err()
 }
 
-func (c RedisCacheConfig) Del(key string) error {
+func (c RedisCache) Del(key string) error {
 	return c.Client.Del(ctx, key).Err()
 }
